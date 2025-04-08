c679a8808339b3508c098cfd5b0fe0679497dfbc
olarik surinta <35139872+mrolarik@users.noreply.github.com>
2025-04-08 21:26:38 +0700

    Update app_5.py

--- app_5.py
+++ app_5.py
@@ -6,7 +6,7 @@
 
 #target_options = {
 #    "Target 1": "https://image-cdn.essentiallysports.com/wp-content/uploads/2024-02-16T010328Z_1841023319_MT1USATODAY22532030_RTRMADP_3_MLS-PRESEASON-NEWELLS-OLD-BOYS-AT-INTER-MIAMI-CF.jpg",
-#    "Target 2": "https://st4.depositphotos.com/19858006/23299/v/450/depositphotos_232996766-stock-illustration-milan-italy-december-2018-vector.jpg",
+#    "Target 2": "https://static.vecteezy.com/system/resources/previews/021/066/020/non_2x/set-of-popular-sportswear-logos-free-vector.jpg",
 #    "Target 3": "https://upload.wikimedia.org/wikipedia/commons/7/70/Sparrow_on_branch.jpg"
 #}
 
